9e45cfc25696cb4694512da8263567d876d17c28
chucongqing <chucongqing@gmail.com>
2023-07-18 06:50:21 -0700

    fix

--- lua/ccq-nvim.lua
+++ lua/ccq-nvim.lua
@@ -2,7 +2,7 @@ vim.notify("Hello, world! helllo333333333333333", "info")
 
 local mt = {}
 
-returm setmetatable({
+return setmetatable({
   setup = function()
     vim.notify("ccq-vim setup", "info")
   end
